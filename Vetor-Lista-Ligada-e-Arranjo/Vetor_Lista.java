e80118ea3c995388d2436b72b42b160526307439
Eduardo Medeiros <eduardo.aguiar@escolar.ifrn.edu.br>
2025-11-08 11:44:09 -0300

    corrigindo a implementação de vetor em lista duplamente ligada, também faltando classe de testes

--- Vetor-Lista-Ligada-e-Arranjo/Vetor_Lista.java
+++ Vetor-Lista-Ligada-e-Arranjo/Vetor_Lista.java
@@ -36,14 +36,12 @@ public class Vetor_Lista {
             if(isEmpty()){
                 throw new EVetorVazio("Não é possível mostrar um elemento de um vetor vazio");
             }
-            if (index < 0 || index > this.size){
+            if (index < 0 || index >= this.size){
                 throw new EVetorIndice("Índice inválido");
             }
-            int current_index = 0;
             No current = this.head;
-            while(current_index != index){
+            for (int i = 0; i < index; i++){
                 current = current.next;
-                ++current_index;
             }
             return current.value;
         }
@@ -53,34 +51,53 @@ public class Vetor_Lista {
             if(isEmpty()){
                 throw new EVetorVazio("Não é possível remover um elemento de um vetor vazio");
             }
-            if (index < 0 || index > this.size){
+            if (index < 0 || index >= this.size){
                 throw new EVetorIndice("Índice inválido");
             }
-            if (index == 0){
-                Object to_remove = this.head.value;
+            Object to_remove;
+            if (index == 0) {
+                to_remove = this.head.value;
                 this.head = this.head.next;
-                this.head.prev = null;
+                if (this.head == null){
+                    this.tail = this.head;
+                }else{
+                    this.head.prev = null;
+                }
                 this.size--;
                 return to_remove;
             }
-            if (index == this.size - 1){
-                Object to_remove = this.tail.value;
+            if (index == this.size - 1) { 
+                to_remove = this.tail.value;
                 this.tail = this.tail.prev;
-                this.tail.next = null;
+                if (this.tail == null){
+                    this.head = this.tail;
+                }else{
+                    this.tail.next = null; 
+                }
                 this.size--;
                 return to_remove;
             }
-            Object to_remove = elemAtRank(index);
-            int current_index = 0;
-            No current = this.head;
-            while (current.value != to_remove || current_index != index){
-                current = current.next;
-                current_index++;
+            if (index < this.size / 2){
+                No current = this.head;
+                for (int i = 0; i < index; ++i){
+                    current = current.next;
+                }
+                to_remove = current.value;
+                current.prev.next = current.next;
+                current.next.prev = current.prev;
+                this.size--;
+                return to_remove;
+            }else{
+                No current = this.tail;
+                for (int i = this.size - 1; i > index; --i){
+                    current = current.prev;
+                }
+                to_remove = current.value;
+                current.prev.next = current.next;
+                current.next.prev = current.prev;
+                this.size--;
+                return to_remove;
             }
-            current.prev.next = current.next;
-            current.next.prev = current.prev;
-            this.size--;
-            return to_remove;
         }
         
         @Override
@@ -93,32 +110,52 @@ public class Vetor_Lista {
                     to_add.value = o;
                     to_add.prev = null;
                     to_add.next = this.head;
-                    this.head.prev = to_add;
+                    if (this.head == null){
+                        this.tail = to_add;
+                    }else{
+                        this.head.prev = to_add;
+                    }
                     this.head = to_add;
                     this.size++;
                     return;
                 }
-                if (index == this.size - 1){
+                if (index == this.size){
                     No to_add = new No();
                     to_add.value = o;
                     to_add.next = null;
                     to_add.prev = this.tail;
-                    this.tail.next = to_add;
+                    if (this.tail == null){
+                        this.head = to_add;
+                    }else{
+                        this.tail.next = to_add; 
+                    }
                     this.tail = to_add;
                     this.size++;
                     return;
                 }
                 No to_add = new No();
                 to_add.value = o;
-                No current = this.head;
-                for(int i = 0; i < index; ++i){
-                    current = current.next;
+                if (index < this.size / 2){
+                    No current = this.head;
+                    for(int i = 0; i < index; ++i){
+                        current = current.next;
+                    }
+                    to_add.next = current;
+                    to_add.prev = current.prev;
+                    current.prev.next = to_add;
+                    current.prev = to_add;
+                    this.size++;
+                }else{
+                    No current = this.tail;
+                    for(int i = this.size - 1; i > index; --i){
+                        current = current.prev;
+                    }
+                    to_add.next = current;
+                    to_add.prev = current.prev;
+                    current.prev.next = to_add;
+                    current.prev = to_add;
+                    this.size++;
                 }
-                to_add.next = current;
-                to_add.prev = current.prev;
-                current.prev.next = to_add;
-                current.prev = to_add;
-                this.size++;
             }
         }
         
@@ -127,16 +164,17 @@ public class Vetor_Lista {
             if(isEmpty()){
                 throw new EVetorVazio("Não é possível substituir elemento de um vetor vazio");
             }
-            if (index < 0 || index > this.size){
+            if (index < 0 || index >= this.size){
                 throw new EVetorIndice("Índice inválido");
             }
-            Object old_element = elemAtRank(index);
+            Object old_element;
             No current = this.head;
-            while(current.value != old_element){
+            for (int i = 0; i < index; ++i){
                 current = current.next;
             }
+            old_element = current.value;
             current.value = o;
-            return  old_element;
+            return old_element;
         }
     }
 
