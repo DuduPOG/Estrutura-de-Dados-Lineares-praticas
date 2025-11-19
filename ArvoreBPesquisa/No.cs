b15f8157cc6410c076a6747789b2163accb34140
Eduardo Medeiros <eduardo.aguiar@escolar.ifrn.edu.br>
2025-11-19 17:05:20 -0300

    Iniciando a implementação da árvore binária de pesquisa, ainda irei implementar uma árvore genérica para ter menos trabalho usando herança nas árvores seguintes

---
+++ ArvoreBPesquisa/No.cs
@@ -0,0 +1,55 @@
+﻿public class No
+{
+    private No pai;
+    private No FE;
+    private int value;
+    private No FD;
+
+    public No()
+    {
+        this.pai = null;
+        this.FE = null;
+        this.value = null;
+        this.FD = null;
+    }
+
+    public setPai(No n)
+    {
+        this.pai = n;
+    }
+
+    public setFE(No n)
+    {
+        this.FE = n;
+    }
+
+    public setValue(No n)
+    {
+        this.value = n;
+    }
+
+    public setFD(No n)
+    {
+        this.FD = n;
+    }
+
+    public getPai()
+    {
+        return this.pai;
+    }
+
+    public getFE()
+    {
+        return this.FE;
+    }
+
+    public getValue()
+    {
+        return this.value;
+    }
+
+    public getFD()
+    {
+        return this.FD;
+    }
+}
